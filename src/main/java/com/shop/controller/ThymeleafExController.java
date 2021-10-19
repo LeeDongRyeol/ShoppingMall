986bdaad33d63e8a5779cb3a7075aeee8f0acdf1
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2021-10-20 01:14:12 +0900

    타임리프 예제5 링크페이지

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -76,4 +76,9 @@ public class ThymeleafExController {
 
         return "thymeleafEx/thymeleafEx04";
     }
+
+    @GetMapping(value = "/ex05")
+    public String thymeleafExample05() {
+        return "thymeleafEx/thymeleafEx05";
+    }
 }
